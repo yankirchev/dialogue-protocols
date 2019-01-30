7716b949688eec3bf8002a80f76b035fcf79f7a5
yankirchev <yan.kirchev@icloud.com>
2019-01-30 12:54:37 +0000

    Implement DeliberationDialogue class template

---
+++ src/app/DeliberationDialogue.js
@@ -0,0 +1,18 @@
+import Dialogue from './Dialogue';
+import { translateTerm } from './helper';
+
+class DeliberationDialogue extends Dialogue {
+  claim(agent, term) {
+
+  }
+
+  concede(agent, term) {
+
+  }
+
+  since(agent, otherAgent, term, justification) {
+
+  }
+}
+
+export default DeliberationDialogue;
