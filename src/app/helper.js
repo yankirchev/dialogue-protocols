fe379a69ea534af564febc6a36ce7bfb3996d692
yankirchev <yan.kirchev@icloud.com>
2019-01-30 12:51:23 +0000

    Implement helper functions

--- src/app/helper.js
+++ src/app/helper.js
@@ -1,17 +1,32 @@
-function translateTerm(term) {
-  console.log('yuh');
+function translate(term) {
+  let translation = decamelise(term.match(/([A-Za-z0-9])+/g)[1]) + ' has property ' + term.match(/([A-Za-z0-9])+/g)[0];
+
+  if (term.match(/([A-Za-z0-9])+/g)[2])
+    translation += ' of value ' + term.match(/([A-Za-z0-9])+/g)[2];
+  if (term.match(/([A-Za-z0-9])+/g)[3])
+    translation += ' and cost ' + term.match(/([A-Za-z0-9])+/g)[3];
+
+  return translation;
 }
 
-function toCamelCase(string) {
+function camelise(string) {
+  return string.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, (match, index) => {
+    if (+match === 0)
+      return ""; // or if (/\s+/.test(match)) for white spaces
 
+    return index === 0 ? match.toLowerCase() : match.toUpperCase();
+  });
 }
 
-function unCamelCase(string) {
-
+function decamelise(string) {
+  return string
+    .replace(/([a-z\d])([A-Z])/g, '$1 $2')
+    .replace(/([A-Z]+)([A-Z][a-z\d]+)/g, '$1 $2')
+    .toLowerCase();
 }
 
 module.exports = {
-  translateTerm,
-  toCamelCase,
-  unCamelCase
+  translate,
+  camelise,
+  decamelise
 }
