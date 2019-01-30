0eeb3b245115d0c1ba8c636527f01c46d1392642
yankirchev <yan.kirchev@icloud.com>
2019-01-30 12:55:09 +0000

    implement PersuasionDialogue class template

---
+++ src/app/PersuasionDialogue.js
@@ -0,0 +1,19 @@
+import Dialogue from './Dialogue';
+import { translate } from './helper';
+import pl from 'tau-prolog';
+
+class PersuasionDialogue extends Dialogue {
+  claim(agent, term) {
+
+  }
+
+  concede(agent, term) {
+
+  }
+
+  question(agent, term) {
+
+  }
+}
+
+export default PersuasionDialogue;
