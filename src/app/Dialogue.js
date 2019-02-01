bf3eccdbcb882b27769839ce78d4fdb34eb82df4
yankirchev <yan.kirchev@icloud.com>
2019-02-01 12:34:26 +0000

    Implement function claim for class Dialogue

--- src/app/Dialogue.js
+++ src/app/Dialogue.js
@@ -1,5 +1,5 @@
 import pl from 'tau-prolog';
-import { translateTerm } from './helper';
+import { translate } from './helper';
 
 class Dialogue {
   text = '';
@@ -23,10 +23,33 @@ class Dialogue {
     this.commitmentStoreHistory.push(newCommitmentStoresRecord);
   }
 
+  // Claim(ag_i, l)
   claim(agent, term) {
+    /*  PRE-CONDITIONS */
 
+    // demo(∏_􏰖ag_i ∪ Com_ag_i, l)
+    const session = pl.create(1000);
+    session.consult(agent.knowledgeBase + agent.commitmentStore);
+    session.query(term);
+
+    if (!session.answer) {
+      throw new Error(`Pre-conditions of claimimg "${translate(term)}" by ${agent} are not satisfied because \
+                      the agent cannot demonstrate the claim through their commitment store and/or knowledge base!`);
+    }
+
+    // ¬l ∈ Com_ag_j for any ag_j ∈ Ag
+    for (const anyAgent in this.agents) {
+      if (anyAgent.commitmentStore.contains(term)) {
+        throw new Error(`Pre-conditions of claimimg "${translate(term)}" by ${agent} are not satisfied because \
+                        ${anyAgent}'s commitment store already contains the claim!`);
+      }
+    }
+
+    /* POST-CONDITIONS */
+
+    // Com_ag_i ⇒ Com_ag_i ∪ l
+    agent.commitmentStore += term;
   }
-
   why(agent, term) {
 
   }
