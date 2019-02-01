3b3c076845c122afdb6351e6326ef7b1690b9492
yankirchev <yan.kirchev@icloud.com>
2019-02-01 14:48:32 +0000

    Implement function "retract" in class "Dialogue"

--- src/app/Dialogue.js
+++ src/app/Dialogue.js
@@ -48,7 +48,12 @@ class Dialogue {
     /* POST-CONDITIONS */
 
     // Com_ag_i ⇒ Com_ag_i ∪ l
-    agent.commitmentStore += term;
+    agent.commitmentStore += `${term} \n`;
+
+    /* UPDATE DIALOGUE TEXT AND COMMITMENT STORE HISTORY */
+
+    this.text += `${agent.name} claims that ${translate(term)}.\n`
+    this.saveCommitmentStores();
   }
 
   // Why(ag_i, l)
@@ -78,12 +83,47 @@ class Dialogue {
       throw new Error(`Pre-conditions of asking why "${translate(term)}" by ${agent} are not satisfied because \
                       no other agent's commitment store contains the claim!`);
     }
+
+    /* UPDATE DIALOGUE TEXT AND COMMITMENT STORE HISTORY */
+
+    this.text += `${agent.name} asks why is it that ${translate(term)}.\n`
+    this.saveCommitmentStores();
   }
 
+  // Concede(ag_i ,l)
   concede(agent, term) {
+    /* PRE-CONDITIONS */
 
+    // for some agent ag_j /= ag_i, l ∈ Com_ag_j
+    let doesAppear = false;
+
+    for (const someAgent in this.agents) {
+      if (someAgent !== agent && someAgent.commitmentStore.contains(term)) {
+        doesAppear = true;
+      }
+    }
+
+    if (doesAppear === false) {
+      throw new Error(`Pre-conditions of conceding to "${translate(term)}" by ${agent} are not satisfied because \
+                      no other agent's commitment store contains the claim!`);
+    }
+
+    // not (¬l ∈ Com_ag_i )
+    if (agent.commitmentStore.contains(`not(${term})`)) {
+      throw new Error(`Pre-conditions of conceding to "${translate(term)}" by ${agent} are not satisfied because \
+                      the agent's commitment store already contains the negation of the claim!`);
+    }
+
+    /* POST-CONDITIONS */
+
+    // Com_ag_i ⇒ Com_ag_i ∪ l
+    agent.commitmentStore += `${term} \n`;
+
+    /* UPDATE DIALOGUE TEXT AND COMMITMENT STORE HISTORY */
+
+    this.text += `${agent.name} concedes to the claim that ${translate(term)}.\n`
+    this.saveCommitmentStores();
   }
-
   retract(agent, term) {
 
   }
