518507848125326253e993ce61ac527f44179de0
yankirchev <yan.kirchev@icloud.com>
2019-03-09 23:52:37 +0000

    Use "Watson" theme for Carbon Components

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import 'carbon-components/css/carbon-components.css';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
