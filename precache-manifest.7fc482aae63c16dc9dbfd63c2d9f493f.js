3b0417d4eff88b6095996aa8c50e244cc215b7d5
yankirchev <yan.kirchev@icloud.com>
2019-03-10 00:22:13 +0000

    Updates

---
+++ precache-manifest.7fc482aae63c16dc9dbfd63c2d9f493f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61bd4885bd3138b85929",
+    "url": "/diners-discourse/static/js/runtime~main.43f12605.js"
+  },
+  {
+    "revision": "f97792d538d2acb98ecc",
+    "url": "/diners-discourse/static/js/main.4ee60a72.chunk.js"
+  },
+  {
+    "revision": "5ea4f08a8f980b145e24",
+    "url": "/diners-discourse/static/js/2.4cb7256a.chunk.js"
+  },
+  {
+    "revision": "f97792d538d2acb98ecc",
+    "url": "/diners-discourse/static/css/main.57f56f6b.chunk.css"
+  },
+  {
+    "revision": "73f87e57ffdda486a09a9a7661509481",
+    "url": "/diners-discourse/index.html"
+  }
+];
